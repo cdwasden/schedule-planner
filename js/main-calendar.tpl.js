914dd88fd352f37f581a50eff424a96b735b53dc
whauwiller <whauwiller@gmail.com>
2015-03-16 14:59:21 -0600

    Converted Static HTML to Handlebars Template

---
+++ js/main-calendar.tpl.js
@@ -0,0 +1,44 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['main-calendar'] = template({"1":function(depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
+
+  return "                    <div id=\"class_tile_"
+    + alias3(((helper = (helper = helpers.dayIndex || (depth0 != null ? depth0.dayIndex : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"dayIndex","hash":{},"data":data}) : helper)))
+    + "_"
+    + alias3(((helper = (helper = helpers.index || (depth0 != null ? depth0.index : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"index","hash":{},"data":data}) : helper)))
+    + "\" class=\"wl-event non-selected\" style=\"height: "
+    + alias3(((helper = (helper = helpers.cellHeight || (depth0 != null ? depth0.cellHeight : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"cellHeight","hash":{},"data":data}) : helper)))
+    + "px; top: "
+    + alias3(((helper = (helper = helpers.cellTopPos || (depth0 != null ? depth0.cellTopPos : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"cellTopPos","hash":{},"data":data}) : helper)))
+    + "px; left: "
+    + alias3(((helper = (helper = helpers.cellLeftPos || (depth0 != null ? depth0.cellLeftPos : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"cellLeftPos","hash":{},"data":data}) : helper)))
+    + "px;\">\r\n                      <div class=\"wl-event-title\">\r\n                        "
+    + alias3(((helper = (helper = helpers.courseName || (depth0 != null ? depth0.courseName : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"courseName","hash":{},"data":data}) : helper)))
+    + " - "
+    + alias3(((helper = (helper = helpers.sectionNumber || (depth0 != null ? depth0.sectionNumber : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"sectionNumber","hash":{},"data":data}) : helper)))
+    + "\r\n                      </div>\r\n                      <div class=\"wl-event-details\">\r\n                        "
+    + alias3(((helper = (helper = helpers.beginTime || (depth0 != null ? depth0.beginTime : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"beginTime","hash":{},"data":data}) : helper)))
+    + " - "
+    + alias3(((helper = (helper = helpers.endTime || (depth0 != null ? depth0.endTime : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"endTime","hash":{},"data":data}) : helper)))
+    + "\r\n                        <br>\r\n                        "
+    + alias3(((helper = (helper = helpers.room || (depth0 != null ? depth0.room : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"room","hash":{},"data":data}) : helper)))
+    + " "
+    + alias3(((helper = (helper = helpers.building || (depth0 != null ? depth0.building : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"building","hash":{},"data":data}) : helper)))
+    + "\r\n                        <br>\r\n                        "
+    + alias3(((helper = (helper = helpers.instructor || (depth0 != null ? depth0.instructor : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"instructor","hash":{},"data":data}) : helper)))
+    + "\r\n                      </div>\r\n                    </div>\r\n";
+},"3":function(depth0,helpers,partials,data) {
+    return "                            <tr>\r\n                              <th>"
+    + this.escapeExpression(this.lambda(depth0, depth0))
+    + "</th>\r\n                              <td><div>&nbsp;</div></td>\r\n                            </tr>\r\n                            <tr>\r\n                              <th>&nbsp;</th>\r\n                              <td><div>&nbsp;</div></td>\r\n                            </tr>\r\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<div class=\"wrapper-outer\">\r\n    <div id=\"calendar\" class=\"wrapper-inner\">\r\n        <!-- CALENDAR BACKDROP -->\r\n        <table id=\"wl-columns\" cellspacing=\"0\" class=\"wl border-separate wl-border\">\r\n            <thead>\r\n                <tr>\r\n                    <th class=\"wl-border\">&nbsp;</th>\r\n                    <th class=\"wl-border\">Mon</th>\r\n                    <th class=\"wl-border\">Tue</th>\r\n                    <th class=\"wl-border\">Wed</th>\r\n                    <th class=\"wl-border\">Thu</th>\r\n                    <th class=\"wl-border\">Fri</th>\r\n                    <th class=\"wl-border\">Sat</th>\r\n                    <th class=\"wl-border gutter\">&nbsp;</th>\r\n                </tr>\r\n            </thead>\r\n            <tbody>\r\n                <tr>\r\n                    <th class=\"wl-border\">&nbsp;</th>\r\n                    <td class=\"wl-border\"><div class=\"wl-column wl-vspacer\" style=\"height: 1173px;\">&nbsp;</div></td>\r\n                    <td class=\"wl-border\"><div class=\"wl-column\"></div></td>\r\n                    <td class=\"wl-border\"><div class=\"wl-column\"></div></td>\r\n                    <td class=\"wl-border\"><div class=\"wl-column\"></div></td>\r\n                    <td class=\"wl-border\"><div class=\"wl-column\"></div></td>\r\n                    <td class=\"wl-border\"><div class=\"wl-column\"></div></td>\r\n                    <td class=\"wl-border gutter\">&nbsp;</td>\r\n                </tr>\r\n            </tbody>\r\n        </table>\r\n\r\n        <div class=\"wl-event-wrapper\">\r\n            <hr class=\"wl-divider\">\r\n\r\n            <div class=\"wl-events\" style=\"height: 1173px;\">\r\n\r\n                <!-- CLASS CELLS -->\r\n                <div id=\"classes\">\r\n\r\n"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.classes : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "\r\n\r\n                </div>\r\n          \r\n                <!-- CALENDAR BASE STRUCTURE -->\r\n                <div id=\"wl-rows\">\r\n                    <table cellspacing=\"0\" class=\"wl-event-slots\">\r\n                        <tbody>\r\n\r\n"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.hours : depth0),{"name":"each","hash":{},"fn":this.program(3, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "\r\n                        </tbody>\r\n                    </table>\r\n                </div>\r\n            </div>\r\n        </div>\r\n    </div>\r\n</div>";
+},"useData":true});
+})();
--- js/scheduler.js
+++ js/scheduler.js
@@ -1,4 +1,59 @@
 $(document).ready(function(){
+
+    var DOW_INDEX = {"M": 0, "T": 1, "W": 2, "R": 3, "F": 4, "S": 5};
+    var TOP_MIN = 0;
+    var LEFT_MIN = 53;
+    var CELL_HEIGHT = 102;
+    var CELL_WIDTH = 120;
+    var minTime = 2400;
+    var maxTime = 0;
+
+    var rawClassData = {"classes" : [{"index" : 1,"courseName" : "C S360","sectionNumber" : "001","daysTaught" : "T R","beginTime" : "0930","endTime" : "1045","room" : "B114","building" : "MARB","creditType" : "S","instructor" : "Clement"},{"index" : 2,"courseName" : "CSANM452R","sectionNumber" : "001","daysTaught" : "M W","beginTime" : "1300","endTime" : "1450","room" : "1110","building" : "TMCB","creditType" : "S","instructor" : "Adams"},{"index" : 3,"courseName" : "CSANM459R","sectionNumber" : "001","daysTaught" : "T R","beginTime" : "1700","endTime" : "1850","room" : "1110","building" : "TMCB","creditType" : "S","instructor" : "Holladay"},{"index" : 4,"courseName" : "ECON110","sectionNumber" : "002","daysTaught" : "M W F","beginTime" : "0800","endTime" : "0850","room" : "250","building" : "SWKT","creditType" : "S","instructor" : "Pope"},{"index" : 5,"courseName" : "TECH202","sectionNumber" : "002","daysTaught" : "M","beginTime" : "1600","endTime" : "1830","room" : "214","building" : "CTB","creditType" : "S","instructor" : "Davis"}]};
+    var classData = {"classes" : [], "hours": []};
+
+
+    rawClassData.classes.forEach(function(classInfo){
+        var beginTime = parseInt(classInfo.beginTime);
+        var endTime = parseInt(classInfo.endTime);
+        minTime = beginTime < minTime ? beginTime : minTime;
+        maxTime = endTime > maxTime ? endTime : maxTime;
+        var daysTaught = classInfo.daysTaught.split(" ");
+        daysTaught.forEach(function(day){
+            classData.classes.push({})
+            $.extend(classData.classes[classData.classes.length - 1], classInfo);
+            classData.classes[classData.classes.length - 1].dayIndex = DOW_INDEX[day];
+        });
+    });
+
+    for(var t = Math.floor(minTime / 100); t < (maxTime / 100); t++){
+        if(t < 12){
+            classData.hours.push(t + "am");
+        } else if (t > 12){
+            classData.hours.push((t % 12) + "pm");    
+        } else {
+            classData.hours.push("12pm");
+        }
+    }
+
+    classData.classes.forEach(function(classInfo){
+        var beginTime = parseInt(classInfo.beginTime);
+        var numericBeginTime = Math.floor(beginTime / 100) + ((beginTime % 100) / 60);
+        var classDuration = parseInt(classInfo.endTime) - beginTime;
+        var numericClassDuration = Math.floor(classDuration / 100) + ((classDuration % 100) / 60);
+
+        var cellHeight = Math.floor(CELL_HEIGHT * numericClassDuration);
+        var cellTopPos = Math.floor(TOP_MIN + (CELL_HEIGHT * (numericBeginTime - Math.floor(minTime / 100))));
+        var cellLeftPos = Math.floor(LEFT_MIN + (CELL_WIDTH * classInfo.dayIndex));
+
+        classInfo.cellHeight = cellHeight;
+        classInfo.cellTopPos = cellTopPos;
+        classInfo.cellLeftPos = cellLeftPos;
+    });
+
+
+    $("#main-calendar").html(Handlebars.templates['main-calendar'](classData));
+
+
     $("#login-button").click(function(e){
         e.preventDefault();
         $("#login-screen").addClass("screen-closed"); 
